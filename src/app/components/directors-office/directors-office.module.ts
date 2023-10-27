130db079158a646f2705df88564a61cffaf4b023
Nikita <nikit15110295@gmail.com>
2023-10-28 01:13:50 +0700

    создан модуль и роутинг для страицы директора (на доработку)

---
+++ src/app/components/directors-office/directors-office.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DirectorRoutingModule } from './directors-office.routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DirectorRoutingModule
+  ]
+})
+export class DirectorsOfficeModule { }
