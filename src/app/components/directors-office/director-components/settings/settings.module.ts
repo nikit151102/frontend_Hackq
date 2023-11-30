03e4440f09caf47b7305d5b55f43fc8e9ff17e84
Nikita <nikit15110295@gmail.com>
2023-11-30 18:26:18 +0700

    + компоненты вкллавок в настройках

--- src/app/components/directors-office/director-components/settings/settings.module.ts
+++ src/app/components/directors-office/director-components/settings/settings.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { SettingsRoutingModule } from './settings.routng.module';
+import { TabViewModule } from 'primeng/tabview';
+import { ProfileSettingComponent } from './setting-components/profile-setting/profile-setting.component';
+import { StaffComponent } from './setting-components/staff/staff.component';
 
 @NgModule({
   declarations: [
-    SettingsComponent
+    SettingsComponent,
+    ProfileSettingComponent,
+    StaffComponent
   ],
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    TabViewModule
   ],
-  exports: [
-    SettingsComponent
-  ]
 })
 export class SettingsModule { }
