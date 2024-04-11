4aa5d0c5ab5b0c5bc70eacdb68f10200f8606f56
nikit15110295@gmail.com <60881026+nikit151102@users.noreply.github.com>
2024-04-11 17:30:28 +0700

    подправки шапки главной страницы

--- src/app/components/home/home-components/section-one/section-one.component.ts
+++ src/app/components/home/home-components/section-one/section-one.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { DialogNewApplicationComponent } from '../dialog-new-application/dialog-new-application.component';
-
+import { ButtonModule } from 'primeng/button';
+import { NewApplicationModule } from '../../../new-application/new-application.module';
 @Component({
     selector: 'app-section-one',
     templateUrl: './section-one.component.html',
     styleUrls: ['./section-one.component.css'],
     standalone: true,
-    imports: [DialogNewApplicationComponent]
+    imports: [ButtonModule,DialogNewApplicationComponent, NewApplicationModule]
 })
 export class SectionONEComponent {
 
